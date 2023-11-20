a05fc29009d61154f9443c2d3193ef4c22ffa5c5
Nodaka <haruka.nodaka@gmail.com>
2023-11-20 23:25:54 +0900

    upload

---
+++ nbtof_generate.py
@@ -0,0 +1,21 @@
+import nbtof_base
+import nbtof_concat
+
+def nbtof_generate(
+    output_py_file,
+    notebook_file,
+    ):
+    
+    with open(output_py_file, 'w') as f:
+        pass
+    
+    if type(notebook_file) == str:
+        notebook_file = [notebook_file]
+    
+    for notebook_file_name in notebook_file:
+        func_file_path, _ = nbtof_base.nbtof_base(nb_path=notebook_file_name)
+        nbtof_concat.nbtof_update_base(
+            output_py_file,
+            func_file_path)
+
+    return output_py_file
